9b1e0a06bbdfcefb9b207bca0456facb9c6ca7f6
AD <silentflute.ad@gmail.com>
2025-05-14 09:17:08 +0100

    add wallet model, with demos

--- Projects/ErrorHandlingDemo.playground/Contents.swift
+++ Projects/ErrorHandlingDemo.playground/Contents.swift
@@ -33,17 +33,76 @@ extension NumericErrorDelegate : LocalizedError {
     }
 }
 
+/// Wallet model
 struct Wallet {
     private var _coins: Int
+    public var balance: Int {
+        get { return self._coins }
+    }
     
-    public init(with amount: Int = 0) {
+    init(deposit amount: Int = 0) {
+        print ("Depositing: \(amount) into wallet")
         self._coins = amount
     }
     
-    public var balance: Int {
-        return self._coins
+    mutating func credit(_ amount: Int = 0) throws {
+        guard amount > 0 else {
+            throw NumericErrorDelegate.mustBePositive
+        }
+        
+        handleCredit(amount)
+    }
+    
+    mutating func debit(_ amount: Int = 0) throws {
+        guard amount > 0 else {
+            throw NumericErrorDelegate.mustBePositive
+        }
+        guard (_coins >= amount) else {
+            throw NumericErrorDelegate.notEnoughFunds(funds: _coins)
+        }
+        guard ((_coins - amount) >= 0) else {
+            throw NumericErrorDelegate.notEnoughFunds(funds: _coins)
+        }
+        
+        handleDebit(amount)
+    }
+    
+    private mutating func handleCredit(_ amount: Int) {
+        print ("Credited: \(amount) into wallet")
+        self._coins += amount
+    }
+    
+    private mutating func handleDebit(_ amount: Int) {
+        print ("Debited: \(amount) from wallet")
+        self._coins -= amount
     }
 }
+
+print ("Demo #1 - Do-try-catch block")
+print ("\n--------------------\n")
+
+let deposit: Int = 100
+var wallet = Wallet(deposit: deposit)
+
+do {
+    try wallet.credit(50)  // will succeed
+    try wallet.credit(-100) // will throw
+    try wallet.debit(-50) // will throw
+    try wallet.credit(0) // will throw
+    try wallet.debit(0) // will throw
+    try wallet.debit(25)  // will succeed
+} catch let err {
+    print ("\n Error -- \(err.localizedDescription)")
+}
+
+print ("\n Balance: \(wallet.balance)")
+
+print ("Demo #2 - More complex do-try-catch block")
+print ("\n--------------------\n")
+
+
+print ("Demo #3 - Use a result tuple")
+print ("\n--------------------\n")
 
 
 print ("\n--------------------\n")
